6931bbfe68bfb0dfd9faa0c60d88237b38d39910
mercenarioz <funnystar320@gmail.com>
2024-10-01 22:26:10 +0700

    feat: add masthead

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "My personal blog";
 export const SITE_DESCRIPTION = "Welcome, this is my personal blog";
 
+export const INSTAGRAM_URL = "https://www.instagram.com/vu.le1352";
