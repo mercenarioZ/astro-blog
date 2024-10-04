a4dac056bb1abeace21f6d7c51fbf3b42e5a9cf7
mercenarioz <funnystar320@gmail.com>
2024-10-04 13:29:19 +0700

    chore(config): simplify config file

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "My personal blog";
-export const SITE_DESCRIPTION = "Welcome, this is my personal blog";
+export const SITE_TITLE = "";
+export const SITE_DESCRIPTION = "";
 
 export const INSTAGRAM_URL = "https://www.instagram.com/vu.le1352";
