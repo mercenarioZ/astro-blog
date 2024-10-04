41167592e9cd3dcb50e6f19f6a5edf8e53928799
mercenarioz <funnystar320@gmail.com>
2024-10-04 11:51:09 +0700

    fix(assets): images are not be displayed on production

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()]
