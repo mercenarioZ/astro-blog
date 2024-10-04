58d2df243396b79615087bdabb839d5df9af6569
mercenarioz <funnystar320@gmail.com>
2024-10-04 14:41:06 +0700

    feat(seo): config meta tags for SEO

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "";
-export const SITE_DESCRIPTION = "";
+export const SITE_DESCRIPTION = "A small blog about programming and life";
 
 export const INSTAGRAM_URL = "https://www.instagram.com/vu.le1352";
