db87e1f757ed3a97664d8fe55b418821d7452fe4
mercenarioZ <funnystar320@gmail.com>
2026-04-02 16:04:02 +0700

    fix: upgrade Astro version, restore Tailwind styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
-import tailwind from '@astrojs/tailwind';
-
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
